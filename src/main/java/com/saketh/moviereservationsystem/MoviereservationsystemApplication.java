690a1ddb288f8c94d7056b5690c4058da95b0867
Saketh Balijepalli <sakethbalijepalli@MacBookPro.home.local>
2026-07-10 15:05:57 -0500

    Initial commit

---
+++ src/main/java/com/saketh/moviereservationsystem/MoviereservationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.saketh.moviereservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviereservationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviereservationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saketh/moviereservationsystem/MoviereservationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saketh.moviereservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereservationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
